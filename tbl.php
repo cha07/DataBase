4348f8f42562b403f5fc2c57815ab6f2c1c559a8
cha07 <charlotte.t@codeur.online>
2018-05-15 16:26:22 +0200

    first commit

--- tbl.php
+++ tbl.php
@@ -2,7 +2,7 @@
 
 try
 {
-	$db = new PDO('mysql:host=localhost;dbname=CSV_DB;charset=utf8', 'root', 'root');
+	$db = new PDO('mysql:host=localhost;dbname=CSV_DB;charset=utf8', '', '');
 }
 catch(Exception $e)
 {
